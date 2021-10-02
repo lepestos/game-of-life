cf023d622ace3036cfffa9f5ca90bbda6413db7e
Kirill Nazarov <kirillnazarov1928@gmail.com>
2021-10-02 18:12:24 +0300

    Made it user-friendly

--- static/js/field.js
+++ static/js/field.js
@@ -1,39 +1,88 @@
 $(document).ready(function () {
-    var board = $('.container');
+    var board = $('#board');
+    var iboard = $('#iboard');
     var nextgen = $('#nextgen');
+    var new_state = $('#new_state')
+    var dims = $('#dims')
     var color;
-    makeGrid(n, m);
+    makeGrid(n, m, state);
+    makeInteractiveGrid(n, m, state);
 
-    function makeGrid(n, m){
+    function makeGrid(n, m, state){
         board.html('');
         board.css("width", m * 30+2);
         for (var i = 0; i < n; i++) {
             for (var j = 0; j < m; j++){
-                color = chooseColor(i, j);
-                makeSquare(color);
+                color = chooseColor(i, j, state);
+                makeSquare(board, color);
+            }
+        }
+    }
+
+    function makeInteractiveGrid(n, m){
+        iboard.html('')
+        iboard.css("width", m * 30+2);
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < m; j++){
+                color = 'white';
+                makeSquare(iboard, color, i*m + j);
+                $("#sq" + (i*m + j)).click(function(event){
+                    $('#' + (event.target.id)).toggleClass('white black')
+                });
             }
         }
 
     }
 
-    function makeSquare(color) {
-        board.append('<div class="square ' + color + '"></div>');
+    function makeSquare(element, color, id=null) {
+        if (id === null) {
+            element.append('<div class="square ' + color + '"></div>');
+        }
+        else {
+            element.append('<div class="square ' + color + '" id="sq' + id +'"></div>');
+        }
     }
 
-    function chooseColor(i, j) {
+    function chooseColor(i, j, state) {
         var k = i * m + j;
-        if ((1 << k) & state){
+        if (1n << BigInt(k) & state){
             return 'black'
         }
         return 'white'
     }
 
     nextgen.click(function(){
-        $.getJSON($SCRIPT_ROOT + '/_next_state',{state: state, n: n, m: m},
+        $.getJSON($SCRIPT_ROOT + '/_next_state',{state: state, n: n, m: m, nxt: 1},
             function(data){
-                state = data.state;
-                makeGrid(n, m);
+                var next_state = BigInt(data.state);
+                makeGrid(n, m, next_state);
+                state = next_state;
             })
-    })
+    });
+
+    new_state.click(function(){
+        var new_state = 0n;
+        var i = 0n;
+        var val;
+        iboard.children().each(function(){
+            val = ($(this).attr("class").split(/\s+/)[1]==='black') ? 1n : 0n;
+            new_state += (val << i);
+            i += 1n;
+        })
+        $.getJSON($SCRIPT_ROOT + '/_next_state',{state: new_state, n: n, m: m, nxt: 0},
+            function(data){
+                new_state = BigInt(data.state);
+                state = new_state;
+                makeGrid(n, m, new_state);
+        })
+    });
+
+    dims.click(function(){
+        n = $('#n').val();
+        m = $('#m').val();
+        makeInteractiveGrid(n, m);
+        makeGrid(n, m, state)
+    });
+
 
 })
--- views.py
+++ views.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, redirect,\
     url_for, session, request, jsonify
 from flask_bootstrap import Bootstrap
 
-from forms import GameForm
 from logic import Game
 
 app = Flask(__name__)
@@ -15,23 +14,16 @@ app.secret_key = os.environ.get('SECRET_KEY', 'dev')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    form = GameForm()
-    if form.validate_on_submit():
-        state = form.state.data
-        game = Game.from_str(state, sep='\r\n')
-        session['n'] = game.n
-        session['m'] = game.m
-        session['state'] = game.binary_state()
-        return redirect(url_for('index'))
-    n, m, state = session.get('n'), session.get('m'), session.get('state')
-    return render_template("index.html",form=form,
-                           n=n, m=m, state=state)
+    return render_template("index.html")
 
 @app.route('/_next_state')
 def get_next_state():
     n = request.args.get('n')
     m = request.args.get('m')
     state = request.args.get('state')
+    nxt = request.args.get('nxt')
     game = Game.from_number(int(state), int(n), int(m))
-    game.next_state()
-    return jsonify(n=game.n, m=game.m, state=game.binary_state())
+    if int(nxt):
+        game.next_state()
+    print(game.binary_state())
+    return jsonify(n=game.n, m=game.m, state=str(game.binary_state()))
